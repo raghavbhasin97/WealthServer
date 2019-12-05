ad37f7420b9cf7a1f189acde8857d0ff288af6d3
Raghav Bhasin <raghavbhasin97@gmail.com>
2019-12-04 20:55:56 -0500

    Inital commit
    Added Bare bones of a node.js api.

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+// Load Environment
+require('dotenv').config();
+
+// Start the app.
+const app = express()
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+const mongoURI = process.env.ENV == 'TEST' ? process.env.MONGODB_TEST_URI : process.env.MONGODB_URI
+mongoose
+	.connect(
+		mongoURI, 
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log('Connected to MongoDB'))
+	.catch( err => console.log(`Problem connecting ${err}`))
+
+// // Passport middleware
+// app.use(passport.initialize());
+const port = process.env.PORT || 5000
+
+app
+	.listen(
+		port, 
+		() => { console.log(`Server running on port ${port}`) }
+	)
