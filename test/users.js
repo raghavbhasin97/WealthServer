a161f7f91b39c21ebcb324a72cbe00f85ae6792a
Raghav Bhasin <raghavbhasin97@gmail.com>
2019-12-05 20:37:20 -0500

    Added Tests for login

--- test/users.js
+++ test/users.js
@@ -22,7 +22,7 @@ describe('User', () => {
   /*
   * Test the /POST route
   */
-  describe('/POST user', () => {
+  describe('/POST api/users/', () => {
     // Test All missing fields.
       it('it should not POST a user without name, email and password field', (done) => {
           let user = {}
@@ -185,6 +185,176 @@ describe('User', () => {
                   res.should.have.status(201);
                   res.text.should.eq('User created')
               done();
+            });
+      });
+
+  });
+   /*
+  * Test the login  route
+  */
+  describe('/GET api/users/login', () => {
+      // Test All missing fields.
+      it('it should not login a user without email and password field', (done) => {
+          let user = {}
+        chai.request(server)
+            .get('/api/users/login')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.json;
+                  res.should.have.status(400);
+                  res.body.should.be.a('object');
+                  assert.equal(Object.keys(res.body).length, 2);
+                  res.body.should.have.property('email').eql('Email field is required');
+                  res.body.should.have.property('password').eql('Password field is required');
+              done();
+            });
+      });
+
+      // Test All missing fields one by one.
+      it('it should not login a user without password field', (done) => {
+          let user = {password: '123445'}
+        chai.request(server)
+            .get('/api/users/login')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.json;
+                  res.should.have.status(400);
+                  res.body.should.be.a('object');
+                  assert.equal(Object.keys(res.body).length, 1);
+                  res.body.should.have.property('email').eql('Email field is required');
+              done();
+            });
+      });
+
+      it('it should not login a user without email field', (done) => {
+          let user = {email: 'jdoe@email.com'}
+        chai.request(server)
+            .get('/api/users/login')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.json;
+                  res.should.have.status(400);
+                  res.body.should.be.a('object');
+                  assert.equal(Object.keys(res.body).length, 1);
+                  res.body.should.have.property('password').eql('Password field is required');
+              done();
+            });
+      });
+
+      // Test field validations
+      it('it should not login a user without valid email', (done) => {
+          let user = {
+            email: 'jdoe',
+            password: '123445'
+          }
+        chai.request(server)
+            .get('/api/users/login')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.json;
+                  res.should.have.status(400);
+                  res.body.should.be.a('object');
+                  assert.equal(Object.keys(res.body).length, 1);
+                  res.body.should.have.property('email').eql('Email is invalid');
+              done();
+            });
+      });
+
+      it('it should not login a user without password match', (done) => {
+          let user = {
+              password: "123456",
+              name: "John Doe",
+              email: "jdoe@email.com"
+          }
+          // Create a user to use.
+        chai.request(server)
+            .post('/api/users')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.html;
+                  res.should.have.status(201);
+                  res.text.should.eq('User created')
+                  let wrongUser = {
+                    email: 'jdoe@email.com',
+                    password: "1234456"
+                  }
+                  chai.request(server)
+                      .get('/api/users/login')
+                      .send(wrongUser)
+                      .end((err, res) => {
+                            res.should.be.json;
+                            res.should.have.status(400);
+                            res.body.should.be.a('object');
+                            assert.equal(Object.keys(res.body).length, 1);
+                            res.body.should.have.property('password').eql('Password incorrect');
+                        done();
+                      });
+            });
+      });
+
+      // Test User Not Found
+      it('it should not login if user email not found in DB', (done) => {
+          let user = {
+              password: "123456",
+              name: "John Doe",
+              email: "jdoe@email.com"
+          }
+          // Create a user to use.
+        chai.request(server)
+            .post('/api/users')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.html;
+                  res.should.have.status(201);
+                  res.text.should.eq('User created')
+                  let wrongUser = {
+                    email: 'jdoe1@email.com',
+                    password: "123456"
+                  }
+                  chai.request(server)
+                      .get('/api/users/login')
+                      .send(wrongUser)
+                      .end((err, res) => {
+                            res.should.be.json;
+                            res.should.have.status(404);
+                            res.body.should.be.a('object');
+                            assert.equal(Object.keys(res.body).length, 1);
+                            res.body.should.have.property('email').eql('User not found');
+                        done();
+                      });
+            });
+      });
+
+      // Test Login Success
+      it('it should login a valid user', (done) => {
+          let user = {
+              password: "123456",
+              name: "John Doe",
+              email: "jdoe@email.com"
+          }
+          // Create a user to use.
+        chai.request(server)
+            .post('/api/users')
+            .send(user)
+            .end((err, res) => {
+                  res.should.be.html;
+                  res.should.have.status(201);
+                  res.text.should.eq('User created')
+                  let wrongUser = {
+                    email: 'jdoe@email.com',
+                    password: "123456"
+                  }
+                  chai.request(server)
+                      .get('/api/users/login')
+                      .send(wrongUser)
+                      .end((err, res) => {
+                            res.should.be.json;
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            assert.equal(Object.keys(res.body).length, 1);
+                            res.body.should.have.property('token').include('Bearer');
+                        done();
+                      });
             });
       });
 
